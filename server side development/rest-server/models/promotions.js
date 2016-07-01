ef8b4ec9fd5b60c96de28fdb14cde09e5bd38c78
Shuwei <sfang@thoughtworks.com>
2016-07-01 19:18:43 +0800

    rest server

---
+++ server side development/rest-server/models/promotions.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var promotionSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	image: {
+		type: String,
+		required: true
+	},
+	label: {
+		type: String,
+		default: ""
+	},
+	price: {
+		type: Currency,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	}
+});
+
+var Promotions = mongoose.model("Promotion", promotionSchema);
+module.exports = Promotions;
